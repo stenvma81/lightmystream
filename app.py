7d0674e99e9b6e8d631bfddb1d331dc7df2cec24
Matti Stenvall <matti.stenvall@gmail.com>
2024-01-08 11:55:00 +0200

    Updated app.py to test streamlit with mock data

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
 
-# Streamlit app code
-st.title("My Streamlit App")
-st.write("Hello, Streamlit!")
+
+@st.cache_data
+def get_data_from_excel():
+    dataframe = pd.read_excel("testsheet.xlsx")
+
+    return dataframe
+
+
+test_data = get_data_from_excel()
+
+
+st.title("Testing streamlit charts")
+st.write("Test data: ")
+st.write(test_data)
+
+
+fig = px.line(test_data, x="Date", y="Value", title="Test Line Chart")
+st.plotly_chart(fig)
